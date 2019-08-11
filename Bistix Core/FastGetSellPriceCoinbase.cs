a85999efa2166605d283de5407d921991cccdd4f
Hayk Jomardyan <jomardyan@outlook.com>
2019-08-11 17:01:56 +0200

    New Features

---
+++ Bistix Core/FastGetSellPriceCoinbase.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Reflection;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Bistix_Core
+{
+    internal class FastGetSellPriceCoinbase
+    {
+        //EUR
+        public double BTCEURPRICE;
+
+        public double LTCEURPRICE;
+
+        //USD
+        public double BTCUSDPRICE;
+
+        public double LTCUSDPRICE;
+
+        public void GetPrice(TextBlock textblock, string crypto, string currency)
+
+        {
+            string className = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+            string jsondata = new WebClient().DownloadString($"https://api.coinbase.com/v2/prices/{crypto}-{currency}/sell");
+            CoinBaseMainData data = JsonConvert.DeserializeObject<CoinBaseMainData>(jsondata);
+
+            if (currency == "USD")
+            {
+                textblock.Text = data.Data.Amount + " $";
+            }
+            else if (currency == "EUR")
+            {
+                textblock.Text = data.Data.Amount + " €";
+            }
+
+            //Set Price Property
+
+            if (crypto == "BTC" & currency == "EUR")
+            {
+                BTCEURPRICE = data.Data.Amount;
+            }
+            else if (crypto == "LTC" & currency == "EUR")
+            {
+                LTCEURPRICE = data.Data.Amount;
+            }
+            else if (crypto == "BTC" & currency == "USD")
+            {
+                BTCUSDPRICE = data.Data.Amount;
+            }
+            else if (crypto == "LTC" & currency == "USD")
+            {
+                LTCUSDPRICE = data.Data.Amount;
+            }
+        }
+
+        public void SetArrow(MahApps.Metro.IconPacks.PackIconModern icon, double LastPrice, double NewPrice)
+        {
+            if (LastPrice > NewPrice)
+            {
+                SolidColorBrush ColorBrush = new SolidColorBrush();
+                ColorBrush.Color = Color.FromRgb(235, 64, 52);
+                icon.Foreground = ColorBrush;
+                icon.Kind = MahApps.Metro.IconPacks.PackIconModernKind.ArrowDown;
+            }
+            else if (LastPrice < NewPrice)
+            {
+                SolidColorBrush ColorBrush = new SolidColorBrush();
+                ColorBrush.Color = Color.FromRgb(52, 235, 76);
+                icon.Foreground = ColorBrush;
+                icon.Kind = MahApps.Metro.IconPacks.PackIconModernKind.ArrowUp;
+            }
+        }
+    }
+
+    public class CoinBaseData
+    {
+        public string Base { get; set; }
+        public string Currency { get; set; }
+        public double Amount { get; set; }
+    }
+
+    public class CoinBaseMainData
+    {
+        public CoinBaseData Data { get; set; }
+    }
+}
--- Bistix Core/MainWindow.xaml.cs
+++ Bistix Core/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Threading;
 
 namespace Bistix_Core
@@ -10,73 +11,69 @@ namespace Bistix_Core
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double btceurval = 0;
+        private double ltceurval = 0;
+        private double btcusdval = 0;
+        private double ltcusdval = 0;
+
         public MainWindow()
         {
-            double btceurval = 0;
-            double ltceurval = 0;
-            double btcusdval = 0;
-            double ltcusdval = 0;
-
-
             InitializeComponent();
 
-            #region Price-Tick 
-            DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            #region Time-Tick
 
-            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
-            dispatcherTimer.Start();
-            #endregion
-
-            #region Time-Tick 
             DispatcherTimer TimeTimer = new System.Windows.Threading.DispatcherTimer();
             TimeTimer.Tick += new EventHandler(TimeTimer_Tick);
             TimeTimer.Interval = new TimeSpan(0, 0, 1);
             TimeTimer.Start();
-            #endregion
 
-            FastGetSellPrice pricex = new FastGetSellPrice();
-            pricex.GetPrice(BTCEUR_VAL,"BTC", "EUR");
+            #endregion Time-Tick
+
+            FastGetSellPriceCoinbase pricex = new FastGetSellPriceCoinbase();
+            Task[] task = new Task[1];
+            pricex.GetPrice(BTCEUR_VAL, "BTC", "EUR");
             pricex.GetPrice(LTCEURVAL, "LTC", "EUR");
             pricex.GetPrice(BTCUSD_VAL, "BTC", "USD");
             pricex.GetPrice(LTCUSDVAL, "LTC", "USD");
 
-            void dispatcherTimer_Tick(object sender, EventArgs e)
-            {
-               
-                FastGetSellPrice price = new FastGetSellPrice();
-                //
-                price.GetPrice(BTCEUR_VAL, "BTC", "EUR");
-                price.SetArrow(BTCEURARROW, btceurval, price.BTCEURPRICE);
-                //
-                price.GetPrice(LTCEURVAL, "LTC", "EUR");
-                price.SetArrow(LTCARROW, ltceurval, price.LTCEURPRICE);
-                //
-                price.GetPrice(BTCUSD_VAL, "BTC", "USD");
-                price.SetArrow(BTCUSDARROW, btcusdval, price.BTCUSDPRICE);
-                //
-                price.GetPrice(LTCUSDVAL, "LTC", "USD");
-                price.SetArrow(LTCUSDARROW, ltcusdval, price.LTCUSDPRICE);
-                //
-                btceurval = price.BTCEURPRICE;
-                ltceurval = price.LTCEURPRICE;
-                btcusdval = price.BTCUSDPRICE;
-                ltcusdval = price.LTCUSDPRICE; 
-
-            }
-
             void TimeTimer_Tick(object sender, EventArgs e)
             {
-
-                TimeBOX.Text = DateTime.Now.ToString();
-               
+                TimeBOX.Text = DateTime.Now.ToLongTimeString();
             }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+        }
+
+        private void TagleRefreshData_Checked(object sender, RoutedEventArgs e)
+        {
+            #region Price-Tick
+
+            DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
+            dispatcherTimer.Start();
+
+            #endregion Price-Tick
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            FastGetSellPriceCoinbase price = new FastGetSellPriceCoinbase();
+            price.GetPrice(BTCEUR_VAL, "BTC", "EUR");
+            price.SetArrow(BTCEURARROW, btceurval, price.BTCEURPRICE);
+            price.GetPrice(LTCEURVAL, "LTC", "EUR");
+            price.SetArrow(LTCARROW, ltceurval, price.LTCEURPRICE);
+            price.GetPrice(BTCUSD_VAL, "BTC", "USD");
+            price.SetArrow(BTCUSDARROW, btcusdval, price.BTCUSDPRICE);
+            price.GetPrice(LTCUSDVAL, "LTC", "USD");
+            price.SetArrow(LTCUSDARROW, ltcusdval, price.LTCUSDPRICE);
+
+            btceurval = price.BTCEURPRICE;
+            ltceurval = price.LTCEURPRICE;
+            btcusdval = price.BTCUSDPRICE;
+            ltcusdval = price.LTCUSDPRICE;
         }
     }
 }
